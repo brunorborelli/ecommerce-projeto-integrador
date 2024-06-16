7f4d5d451757abbb0cb545debd5cd3ca1420a9b4
Bruno Borelli <rodriguesborellibruno@gmail.com>
2024-06-16 15:21:11 -0300

    swagger

--- backend/src/main/java/com/ecomerce/backend/controllers/UsuarioController.java
+++ backend/src/main/java/com/ecomerce/backend/controllers/UsuarioController.java
@@ -5,13 +5,19 @@ import com.ecomerce.backend.entities.dtos.UsuarioDTO;
 import com.ecomerce.backend.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
-@Controller
+@RestController
 @RequestMapping(value = "/usuario")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
